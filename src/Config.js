da6a07db3273760e2611b349e98573bfd88eaf77
Laureen Cruz <laureencrz@outlook.com>
2019-08-09 16:49:22 -0700

    finished deployment

--- src/Config.js
+++ src/Config.js
@@ -1,3 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:9090'
+    API_ENDPOINT: 'https://nameless-temple-32242.herokuapp.com/',
+    API_KEY: "ec8a24dc-1d1b-4a9d-946d-30104831ced8",
   }

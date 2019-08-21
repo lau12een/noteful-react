294dd81178058948edaecd183607247df2ffa5c9
Laureen Cruz <laureencrz@outlook.com>
2019-08-20 19:34:30 -0700

    updated config files

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 export default {
     API_ENDPOINT: 'https://nameless-temple-32242.herokuapp.com/',
-    API_KEY: "ec8a24dc-1d1b-4a9d-946d-30104831ced8"
+    API_KEY: "ec8a24dc-1d1b-4a9d-946d-30104831ced8",
+    NODE_ENV: process.env.NODE_ENV || 'development',
   }

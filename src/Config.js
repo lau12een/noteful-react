8cff1de6c0c42ac1c75c528d5262f8c5bca33ac9
Laureen Cruz <laureencrz@outlook.com>
2019-08-22 22:49:13 -0700

    commit for deployment

--- src/Config.js
+++ src/Config.js
@@ -2,6 +2,5 @@ require('dotenv').config();
 export default {
     API_FOLDERS: 'https://nameless-temple-32242.herokuapp.com/api/folders',
     API_NOTES:'https://nameless-temple-32242.herokuapp.com/api/notes',
-    API_KEY: "ec8a24dc-1d1b-4a9d-946d-30104831ced8"
-//    NODE_ENV: process.env.NODE_ENV || 'development',
+    API_KEY: process.env.REACT_APP_API_KEY,
   }
